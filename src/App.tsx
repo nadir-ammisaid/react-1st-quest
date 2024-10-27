179b8376ad74997c86ed817674e3fbb9c648e27a
Nadir AMMISAID <nadir.ammisaid@outlook.com>
2024-10-27 18:20:57 +0100

    quete 4 react finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import PokemonCard from "./Components/PokemonCard";
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   );
 }
