faafadea32a359b404f0be049bf1ea2b43e610f4
Nadir AMMISAID <nadir.ammisaid@outlook.com>
2024-10-29 21:29:12 +0100

    quete react 6 finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import PokemonCard from "./Components/PokemonCard";
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const handleClickPlus = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
+  const handleClickMinus = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
+
   const pokemonList = [
     {
       name: "bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
     },
     {
       name: "mew",
@@ -14,7 +38,18 @@ function App() {
 
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      {pokemonIndex > 0 && (
+        <button type="button" onClick={handleClickMinus}>
+          Précédent
+        </button>
+      )}
+
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button type="button" onClick={handleClickPlus}>
+          Suivant
+        </button>
+      )}
     </div>
   );
 }
